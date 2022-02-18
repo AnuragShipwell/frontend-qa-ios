986ff0283446d53fa1135d0887032f1e89ac38de
AnuragShipwell <anurag@shipwell.com>
2022-02-18 16:18:05 -0600

    changing bundleid for testflight

--- src/tests/login_shipper.test.js
+++ src/tests/login_shipper.test.js
@@ -13,7 +13,7 @@ describe("iOS mobileApp test", () => {
 
 test("==001==> TESTING APP INSTALLATION and VERIFY TEST bundleID", async () => {
   await app.driver.pause(5000)
-  expect(await app.driver.isAppInstalled("com.shipwell")).toBe(true);
+  expect(await app.driver.isAppInstalled("com.shipwell.testflight")).toBe(true);
   await app.driver.queryAppState("com.shipwell")
   console.log("!!!!!!####%%%%%%!!!! app in foregound  !!!!!!####%%%%%%!!!! ")
   await app.driver.pause("5000")
