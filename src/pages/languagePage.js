a2d2e82c8e3c759a91a07106852d7ed94fac4099
AnuragShipwell <anurag@shipwell.com>
2022-02-17 22:18:14 -0600

    increasing language screen timout for test

--- src/pages/languagePage.js
+++ src/pages/languagePage.js
@@ -24,7 +24,7 @@ async languageViewSaveButton(){
     return await this.element('~sendVerification')
 }
 async languageSelectionFlow(){
-    await this.driver.pause(3000)
+    await this.driver.pause(20000)
     const languageViewLanguageInput= await this.languageViewLanguageInput()
     const languageViewEspanolButton= await this.languageViewEspanolButton()
     const languageViewEnglishButton= await this.languageViewEnglishButton()
@@ -32,18 +32,18 @@ async languageSelectionFlow(){
     const languageViewInputBoxEnglish= await this.languageViewInputBoxEnglish()
     const languageViewSaveButton= await this.languageViewSaveButton()
 
-    await languageViewLanguageInput.waitForEnabled({ timeout: 5000 })
+    await languageViewLanguageInput.waitForEnabled({ timeout: 10000 })
     await languageViewLanguageInput.touchAction('tap')
     
-    await languageViewEspanolButton.waitForDisplayed({ timeout: 5000 })
-    await languageViewEnglishButton.waitForEnabled({ timeout: 5000 })
+    await languageViewEspanolButton.waitForDisplayed({ timeout: 10000 })
+    await languageViewEnglishButton.waitForEnabled({ timeout: 10000 })
     await languageViewEnglishButton.touchAction('tap')
-    await languageViewDoneButton.waitForEnabled({ timeout: 5000 })
+    await languageViewDoneButton.waitForEnabled({ timeout: 10000 })
     await languageViewDoneButton.touchAction('tap')
 
-    await languageViewInputBoxEnglish.waitForDisplayed({ timeout: 5000 })
+    await languageViewInputBoxEnglish.waitForDisplayed({ timeout: 10000 })
     
-    await languageViewSaveButton.waitForEnabled({ timeout: 5000 })
+    await languageViewSaveButton.waitForEnabled({ timeout: 10000 })
     await languageViewSaveButton.touchAction('tap')
 }
 
