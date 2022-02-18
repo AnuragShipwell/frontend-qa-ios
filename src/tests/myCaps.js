62fbbef34bf32d03afb4a5dfa0ed202362891e24
AnuragShipwell <anurag@shipwell.com>
2022-02-17 23:39:54 -0600

    Adding auto-launch in caps

--- src/tests/myCaps.js
+++ src/tests/myCaps.js
@@ -8,7 +8,8 @@ let myCaps = {
     //app: "/Users/anuragshipwell/Library/Developer/Xcode/DerivedData/Shipwell-akdpitrjnzikizcqiclbkartdtln/Build/Products/Debug-iphonesimulator/Shipwell.app",
     app: process.env.BITRISE_APP_DIR_PATH,
     xcodeOrgId: "NMS56SGZ6U",
-    xcodeSigningId: "iPhone Developer"
+    xcodeSigningId: "iPhone Developer",
+    autoLaunch: "true"
     },
     connectionRetryTimeout: 240000,
     //host: process.env.HOST|| "0.0.0.0"
